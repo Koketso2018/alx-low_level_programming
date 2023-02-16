da65a34b42e858f7bc9bc50a60c398e4d8cb1061
Koketso2018 <mosenakoketso153@gmail.com>
2023-02-16 14:36:19 -0800

    write a c program that print out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/*
+ * main - returns 0
+ * comment
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
